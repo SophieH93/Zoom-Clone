58e07f0413ea5d69d1c90fbf3997d65ad0d02287
Sophie Hickey <sophiehickey123@gmail.com>
2020-10-05 17:25:32 +0100

    added user mgs/btn functionality

--- server.js
+++ server.js
@@ -15,16 +15,17 @@ app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`);
 })
 
-
 app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room })
 })
-
 
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
         socket.to(roomId).broadcast.emit('user-connected', userId);
+        socket.on('message', message => {
+            io.to(roomId).emit('createMessage', message)
+        })
     })
 })
 
